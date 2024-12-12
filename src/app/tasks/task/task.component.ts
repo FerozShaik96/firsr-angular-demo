f6625ff5a84b16d5498e76df93652ca9090ae6af
FerozShaik96 <ferozshaik1997@gmail.com>
2024-12-13 03:29:03 +0530

    Moved the task into tasks Folder

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { UserTask } from '../user/user.model';
-import { CardComponent } from "../shared/card/card.component";
+import { UserTask } from '../../user/user.model';
+import { CardComponent } from "../../shared/card/card.component";
 import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-task',
